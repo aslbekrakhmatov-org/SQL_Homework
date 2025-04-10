fbc94eb1cbec1211613478b63e48d9ce0eb2322c
aslbekrakhmatov-org <aslbek.rakhmatov.us@gmail.com>
2025-04-10 18:22:37 +0500

    lesson 10 reviewed

--- lesson-10/homework/question.sql
+++ lesson-10/homework/question.sql
@@ -26,4 +26,33 @@ Medians as (
 	(ct.TotalCount%2=1 and Num_Rank = ceiling(ct.TotalCount/2))
 )
 select avg(Num) as Median
-from Medians
+from Medians;
+
+;with TotalDays as(
+	select 1 as n 
+	union all
+	select n+1
+	from TotalDays
+	where n<40
+), 
+ProcessTable as (
+	select td.n as Day, s.num as Shipments
+	from TotalDays td
+	left join Shipments s
+	on td.n = s.n
+),
+RankedTable as (
+	select Num, ROW_NUMBER() over(order by Num) as rn
+	from ProcessTable
+),
+TotalCount as (
+	select count(*) as tc from ProcessTable
+)
+SELECT AVG(CAST(Num AS FLOAT)) AS Median
+FROM RankedTable r
+JOIN TotalCount t ON 1 = 1
+WHERE rn IN (FLOOR((t.tc + 1)/2.0), CEILING((t.tc + 1)/2.0));
+
+
+
+
